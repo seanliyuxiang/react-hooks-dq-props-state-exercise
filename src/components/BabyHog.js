6f80cd6176649ebf4b722eab3cb4aa91e310d4f0
Yuxiang Li <seanliyuxiang@gmail.com>
2021-09-24 14:34:56 -0500

    Added change baby hog's height feature

--- src/components/BabyHog.js
+++ src/components/BabyHog.js
@@ -8,8 +8,10 @@ import GlowingBaby from "../assets/glowing-eyes.png";
 // perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
 
 function BabyHog(props) { 
+
   // destructuring array returned from the useState hook
   const [weight, setWeight] = useState(100);
+  const [height, setHeight] = useState("200px");
 
   // this object has key-value pairs with color pointing to image path
   const eyeColorMapper = {
@@ -19,17 +21,32 @@ function BabyHog(props) {
     "glowing": GlowingBaby
   };
 
-  function handleChangeWeight(e) {
+  function handleChangeWeightAndHeight(e) {
     // how can we reuse this for both buttons?
     // perhaps something with e.target.name === "+"
 
+    const heightAsNumber = parseInt(height.replace('px', '')); // 200
+
     if (e.target.innerText === 'Increase Weight') {
-      // setter function to set weight
+      // setter function to set weight and height
       setWeight(weight + 20);
-    } else { // "Decrease weight" button is clicked
+      setHeight((heightAsNumber + 20) + 'px');
+    } else { // when "Decrease weight" button is clicked
       setWeight(weight - 20);
+      setHeight((heightAsNumber - 20) + 'px');
     }
   }
+
+  // function handleChangeHeight(e) {
+  //   const heightAsNumber = parseInt(height.replace('px', '')); // 200
+
+  //   if (e.target.innerText === 'Increase Weight') {
+  //     // setter function to set height
+  //     setHeight((heightAsNumber + 20) + 'px');
+  //   } else { // "Decrease weight" button is clicked
+  //     setHeight((heightAsNumber - 20) + 'px');
+  //   }
+  // }
 
   return (
     <li className="hogbabies">
@@ -38,13 +55,13 @@ function BabyHog(props) {
       <h3>Hobby: {props.hobby}</h3>
       <h4>Eye Color: {props.eyeColor}</h4>
 
-      <button name="+" onClick={handleChangeWeight}>Increase Weight</button>
-      <button name="-" onClick={handleChangeWeight}>Decrease Weight</button>
+      <button name="+" onClick={handleChangeWeightAndHeight}>Increase Weight</button>
+      <button name="-" onClick={handleChangeWeightAndHeight}>Decrease Weight</button>
 
       <div className="hb-wrap">
         <img
           src={eyeColorMapper[props.eyeColor]}
-          style={{ height: "200px" }}
+          style={{ height: height }}
           alt="MasterBlasterJrJr"
         />
       </div>
